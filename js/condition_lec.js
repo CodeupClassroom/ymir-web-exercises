778954359cad1b7f8c1666d671a6eeaa1fe6254d
Docrob <mark.robinson@codeup.com>
2022-11-28 10:52:40 -0600

    pre condition lecture

---
+++ js/condition_lec.js
@@ -0,0 +1,41 @@
+(function () {
+
+// explain conditionals and the importance of them
+    // control flow and decision making
+
+// if structure
+// magic number game
+// OR an adventure game!!!
+
+// ALSO show choosing cancel in confirm() here and "exiting" the program
+
+// if else
+
+// point out that if the if is false then the ELSE WILL HAPPEN
+
+// if else CHAIN
+
+// SHOW WITHOUT THE ELSE {}
+
+// NEXT REMOVE THE else FROM else if AND SEE WHAT HAPPENS
+
+// can we move the else if and the else around???
+
+// NOW MOVE THIS CODE TO A FUNCTION
+// that processes the user's choice
+// and returns a string that is the result of the user's choice
+// function getMessageFromUserChoice(choice, magicNumber) {
+
+
+
+// ternary
+
+
+// switch
+// rewrite the if-else if chain function using a switch
+
+// remove a break or 2 from the switch and see what happens
+
+
+
+})();
