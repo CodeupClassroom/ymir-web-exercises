ea9cd67457ab6d48ccb08d19fd1ab9ab17214312
Docrob <mark.robinson@codeup.com>
2022-12-05 13:28:30 -0600

    pre obj lec

---
+++ js/object_lec.js
@@ -0,0 +1,48 @@
+"use strict";
+
+(function() {
+    // declare an object use new Object()
+        // use a book
+
+    // declare the same object using object literal notation
+
+    // dynamically assign some properties to it
+        // use dot notation
+            // show off camel case for properties
+
+        // use array notation
+
+        // PLEASE USE DOT NOTATION FOR NOW
+
+    // declare an object with a few properties
+
+    // READ and WRITE object properties
+
+    // put an object in another object
+        // book has author
+
+    // and access the book's author
+
+    // put an array in the book (genres)
+        // and access the genres
+
+    // make an array of books
+        // and access the books
+
+        // push a new book on the array
+
+        // make a function to print out the book titles and author names
+
+    // objects can also have behavior (i.e., functions)
+
+        // what behaviors can a book have???
+            // maybe read() and/or throw()
+
+    // this
+        // refers to the thing that the function belongs to
+            // (or is being called from)
+
+        // rewrite the above functions to use the this keyword
+
+
+})();
