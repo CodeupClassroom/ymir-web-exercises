fff556e75fc0f88053ad80178a9c85f8133642df
Docrob <mark.robinson@codeup.com>
2022-12-06 13:19:03 -0600

    pre math lec

---
+++ js/math.js
@@ -0,0 +1,51 @@
+(function() {
+
+    // explain the Math object
+    // (library of reusable functions that do math things)
+
+    // show Math.random()
+
+        // show Math.random() to get number from 0 to 9
+
+        // add 1 to change it to number from 1 to 10
+
+        // lastly, add floor to "truncate"
+        // actually, see if truncate is a function
+
+
+    // Math.round()
+
+    // show how to use for rounding floats to arbitrary precision
+        // EXPLAIN THE PROCESS
+
+    // Math.floor()
+    // show + and - number floors
+
+    // Math.ceil()
+
+    // Math.pow()
+    // show ES6 shorthand
+
+    // Math.sqrt()
+
+    // Math.PI constant
+
+    // write a cool function to round float to a given precision
+
+
+})();
+
+// let x = 3.14159;
+//
+//
+// console.log(roundToDecimal(x, 10000));
+//
+// function roundToDecimal(number, decimalPlace) {
+//     let y = number * decimalPlace; // changes y to 314.159
+//     y = Math.round(y); // round(314.159) -> 314
+//     y = y / decimalPlace; // changes y to 3.14
+//     return y;
+// }
+//
+// let z = Math.pow(2, 16);
+// z = 2 ** 16;
