d165e0fff66101a9166bb87fd653d6470619c7e3
Docrob <mark.robinson@codeup.com>
2022-12-07 08:12:53 -0600

    post math lec

--- js/math_lec.js
+++ js/math_lec.js
@@ -4,6 +4,7 @@
     // (library of reusable functions that do math things)
 
     // show Math.random()
+    console.log(Math.trunc(Math.random() * 6 + 1));
 
         // show Math.random() to get number from 0 to 9
 
@@ -14,24 +15,45 @@
 
 
     // Math.round()
+    // 3141.59
+    console.log(Math.round(3.14159 * 1000) / 1000);
 
     // show how to use for rounding floats to arbitrary precision
         // EXPLAIN THE PROCESS
 
     // Math.floor()
     // show + and - number floors
+    console.log(Math.floor(-4.2));
 
     // Math.ceil()
 
     // Math.pow()
     // show ES6 shorthand
+    console.log(Math.pow(10, 3));
+    console.log(10 ** 3);
 
     // Math.sqrt()
+    console.log(Math.sqrt(3));
 
     // Math.PI constant
+    console.log(Math.PI);
 
     // write a cool function to round float to a given precision
+    // e.g., roundFloat(3.14159, 3) -> 3.142
+    function roundFloat(floatNum, decimalDigit) {
+        // 1. multiply floatNum by 10 to the power of decimalDigit
+        floatNum = floatNum * (10 ** decimalDigit);
 
+        // 2. call Math.round on floatNum
+        floatNum = Math.round(floatNum);
+
+        // 3. divide floatNum by 10 to the power of decimalDigit
+        floatNum = floatNum / (10 ** decimalDigit);
+
+        return floatNum;
+    }
+
+    console.log(roundFloat(3.14159, 3));
 
 })();
 
