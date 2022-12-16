d47a1c2c502419e5717b46daefa5a4c2b5b14758
Docrob <mark.robinson@codeup.com>
2022-12-15 18:34:17 -0600

    draw rectangle

--- js/draw-rectangle.js
+++ js/draw-rectangle.js
@@ -26,13 +26,14 @@ function drawRectangleTop(width) {
 
 
 function drawRectangleMiddle(width, height) {
-    // output a single line of the middle
+    // output a height - 2 lines of the middle
     for (let i = 0; i < (height - 2); i++) {
         drawRectangleMiddleLine(width);
     }
 }
 
 function drawRectangleMiddleLine(width) {
+    // draw a single line of the middle
     let output = "";
     for (let i = 0; i < width; i++) {
         if(i === 0 || i === (width - 1)) {
